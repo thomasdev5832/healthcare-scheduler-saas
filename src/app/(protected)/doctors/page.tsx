77deec1b7a215cce27beb5e67fae597957c598a4
thomasdev5832 <eugabrielthome@gmail.com>
2025-05-28 20:13:18 -0300

    feat:  ensure user has clinic on doctors page and dashboard

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,9 +1,22 @@
 import { Plus } from "lucide-react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { PageActions, PageContainer, PageContent, PageDescription, PageHeader, PageHeaderContent, PageTitle } from "@/components/ui/page-container"
+import { auth } from "@/lib/auth";
 
-const DoctorsPage = () => {
+const DoctorsPage = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    if (!session?.user) {
+        redirect("/authentication");
+    }
+    if (!session.user.clinic) {
+        redirect("/clinic-form");
+    }
+
     return (
         <PageContainer>
             <PageHeader>
@@ -12,7 +25,7 @@ const DoctorsPage = () => {
                     <PageDescription>Gerencie os médicos da sua clínica.</PageDescription>
                 </PageHeaderContent>
                 <PageActions>
-                    <Button>
+                    <Button className="cursor-pointer">
                         <Plus />
                         Adicionar Médico
                     </Button>
