7109c2b4706689c16acce16372da8750fc1cfeb3
thomasdev5832 <eugabrielthome@gmail.com>
2025-05-28 01:10:49 -0300

    feat: add clinic form page

---
+++ src/actions/create-clinic/index.ts
@@ -0,0 +1,28 @@
+"use server"
+
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { db } from "@/db";
+import { clinicsTable, usersToClinicsTable } from "@/db/schema"
+import { auth } from "@/lib/auth";
+
+export const createClinic = async (name: string) => {
+    const session = await auth.api.getSession({ 
+        headers: await headers(), 
+    });
+    if (!session) {
+        throw new Error("Unauthorized");
+    }
+
+    const [clinic] = await db.insert(clinicsTable).values({name}).returning();
+    if (!clinic) {
+        throw new Error("Failed to create clinic");
+    }
+    // Associate the clinic with the user
+    await db.insert(usersToClinicsTable).values({
+        userId: session.user.id,
+        clinicId: clinic.id,
+    });
+    redirect("/dashboard");
+};
